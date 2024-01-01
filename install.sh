87109ba0725c142674161baf06f8a60a30168570
Aniket <kathareaniket47@gmail.com>
2024-01-01 10:14:30 +0530

    Endeavour

--- install.sh
+++ install.sh
@@ -377,7 +377,7 @@ EOF
 DISTRO=$(awk -F= '/^ID=/{print $2}' /etc/os-release | tr -d '"')
 
 case $DISTRO in
-  arch)
+  arch | endeavour)
     echo -e ":: Distro found ${DISTRO}"
     deps_arch
     ;;
