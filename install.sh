d3d27faf7259ed2fcba4d36c644048b818800a6a
Aniket <kathareaniket@gmail.com>
2024-03-13 18:52:05 +0530

    Manjaro

--- install.sh
+++ install.sh
@@ -377,7 +377,7 @@ EOF
 DISTRO=$(awk -F= '/^ID=/{print $2}' /etc/os-release | tr -d '"')
 
 case $DISTRO in
-  arch | endeavouros)
+  arch | endeavouros | manjaro)
     echo -e ":: Distro found ${DISTRO}"
     deps_arch
     ;;
